2451744dc81ecfaf2ca26cff7d1658a3bf84e027
Spíndola <gabrielhspindola@gmail.com>
2025-08-27 14:41:55 -0300

    update

---
+++ cses_2217.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define srt(x) sort(all(x))
+#define f0r(x, i) for(int i = 0; i < x; i++)
+#define f1r(x, i) for(int i = 1; i < x; i++)
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int n, m; cin >> n >> m;
+    vector<pii> vec(n); 
+    
+    f0r(n, i){
+        cin >> vec[i].first;
+        vec[i].second = i;
+    }
+
+
+    while(m--){
+        int a, b; cin >> a >> b;
+
+        int ia = vec[a-1].second;
+        int ib = vec[b-1].second;
+
+        vec[a-1].swap(vec[b-1]);
+
+        vec[a-1].second = ia;
+        vec[b-1].second = ib;
+
+        f0r(n, i){
+            cerr << vec[i].first;
+        }
+        cerr << endl;
+
+        srt(vec);
+
+        int ans = 1;
+
+        f0r(n-1, i){
+            if(vec[i+1].second < vec[i].second)
+                ans++;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
