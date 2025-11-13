5547cb125feda0d5f0d63d7195b3f5fd7c0cff76
Gabriel <gabrielhspindola@gmail.com>
2025-11-13 16:19:09 -0300

    update

--- cses_1666.cpp
+++ cses_1666.cpp
@@ -4,32 +4,55 @@ using namespace std;
 
 #define all(x) x.begin(), x.end()
 #define srt(x) sort(all(x))
-#define f1r(i, x) for(int i = 1; i < x; i++)
+#define f1r(x) for(int i = 1; i <= x; i++)
 #define vi vector<int>
 #define vll vector<ll>
 #define pii pair<int,int>
 
 typedef long long ll;
 
+vector<vi> grafo;
+vi conexoes, visitados;
+
+void bfs(int n){
+    if(!visitados[n]){
+        queue<int> q;
+        q.push(n);
+        visitados[n] = 1;
+        conexoes.push_back(n);
+
+        while(!q.empty()){
+            int atual = q.front(); q.pop();
+
+            for(auto v: grafo[atual]){
+                if(!visitados[v]){
+                visitados[v] = 1;
+                q.push(v);
+                }
+            }
+        }
+    }
+}   
+
 int main() {
     int n, m; cin >> n >> m;
-    vector<vi> cidade(n+1, vi(n+1, 0));
 
-    int aux = m;
-    while(aux--){
-        int ini, fim; cin >> ini >> fim;
-        cidade[ini][fim]++; cidade[fim][ini]++;
+    grafo.resize(n+1);
+    visitados.resize(n+1);
+
+    while(m--){
+        int a, b; cin >> a >> b;
+        grafo[a].push_back(b);
+        grafo[b].push_back(a);
     }
-
-    for(int i = 1, j = 1; i < n; j++)
-        if(i != j){
-            if(cidade[i][j] == 0){
-                cout << i << " " << j << endl; 
-                i = j;
-            }   
-            else    
-                i++;   
-        }
     
+    f1r(n)
+        bfs(i);
+
+    cout << conexoes.size() - 1 << endl;
+    for (int i = 0; i < conexoes.size() - 1; i++) {
+      cout << conexoes[i] << " " << conexoes[i + 1] << endl;
+    }
+    cout << endl;
     return 0;
 }
---
+++ cses_1671.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define srt(x) sort(all(x))
+#define f0r(x) for(int i = 0; i < x; i++)
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+
+typedef long long ll;
+
+vector<vi> grafo;
+vi distancia;
+
+int main() {
+
+
+    return 0;
+}
